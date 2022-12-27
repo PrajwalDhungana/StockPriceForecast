baa9f81bd48270637f0452adeb63099ab304ef11
Prajwal Dhungana <prajwaldhungana164@gmail.com>
2022-12-27 20:35:50 +0545

    fix: Add Route to handle POST request
    fix: Remove response headers

--- backend/server.py
+++ backend/server.py
@@ -1,35 +1,20 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-@app.after_request
-def set_headers(response):
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Headers"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "*"
-    response.headers['Pragma'] = 'no-cache'
-    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-    response.headers['Expires'] = '0'
-    return response
-
 @app.route('/items')
 def get_items():
     return {"items": ["item1", "item2", "item3"]}
 
-@app.route('/insertTicker', methods=['POST', 'GET'])
-def insertTicker():
-    if request.methods == 'POST':
-        tickerSymbol = request.json['tickerSymbol']
-        return tickerSymbol
-    else:
-        return render_template('')
-
-@app.route("/<ticker>")
-def ticker(ticker):
-    return f"<h1>ticker</h1>"
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.get_json()
+    tickerSymbol = data['tickerSymbol']
+    print(tickerSymbol)
+    return jsonify({'tickerSymbol': tickerSymbol})
 
 if __name__ == '__main__':
     app.run(debug=True)
