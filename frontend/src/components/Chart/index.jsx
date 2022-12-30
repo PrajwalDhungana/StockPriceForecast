3ff7428531aa299fae419f6b9f7852636e683dbd
Prajwal Dhungana <prajwaldhungana164@gmail.com>
2022-12-30 15:38:44 +0545

    Init Chart component

---
+++ frontend/src/components/Chart/index.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import Chart from "react-apexcharts";
+
+const StockChart = () => {
+  const chart = {
+    options: {
+      chart: {
+        id: "basic-bar"
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+      }
+    },
+    series: [
+      {
+        name: "series-1",
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      }
+    ]
+  };
+    return (
+      <div className="app">
+        <div className="row">
+          <div className="mixed-chart">
+            <Chart
+              options={chart.options}
+              series={chart.series}
+              type="bar"
+              width="500"
+            />
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default StockChart;
