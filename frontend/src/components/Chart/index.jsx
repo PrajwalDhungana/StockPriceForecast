93c1052da256acc9da9121c72c2ee35dc67d9305
Prajwal Dhungana <prajwaldhungana164@gmail.com>
2022-12-30 20:32:47 +0545

    feat: Add line area graph

--- frontend/src/components/Chart/index.jsx
+++ frontend/src/components/Chart/index.jsx
@@ -1,37 +1,125 @@
-import React, { Component } from "react";
+import React from "react";
 import Chart from "react-apexcharts";
 
 const StockChart = () => {
-  const chart = {
-    options: {
-      chart: {
-        id: "basic-bar"
-      },
-      xaxis: {
-        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-      }
-    },
+  const variant = {
     series: [
       {
-        name: "series-1",
-        data: [30, 40, 45, 50, 49, 60, 70, 91]
-      }
-    ]
+        name: "AAPL",
+        data: [
+          {
+            x: new Date("2018-03-12").getTime(),
+            y: 76,
+          },
+          {
+            x: new Date("2018-03-13").getTime(),
+            y: 78,
+          },
+          {
+            x: new Date("2018-03-14").getTime(),
+            y: 79,
+          },
+          {
+            x: new Date("2018-03-15").getTime(),
+            y: 69,
+          },
+          {
+            x: new Date("2018-03-16").getTime(),
+            y: 83,
+          },
+          {
+            x: new Date("2018-03-17").getTime(),
+            y: 88,
+          },
+          {
+            x: new Date("2018-03-18").getTime(),
+            y: 81,
+          },
+          {
+            x: new Date("2018-03-19").getTime(),
+            y: 79,
+          },
+          {
+            x: new Date("2018-03-20").getTime(),
+            y: 83,
+          },
+        ],
+      },
+    ],
+    options: {
+      chart: {
+        type: "area",
+        stacked: false,
+        height: 350,
+        width: 700,
+        zoom: {
+          type: "x",
+          enabled: true,
+          autoScaleYaxis: true,
+        },
+        toolbar: {
+          autoSelected: "zoom",
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      markers: {
+        size: 0,
+      },
+      title: {
+        text: "Stock Price Movement",
+        align: "left",
+      },
+      fill: {
+        type: "gradient",
+        gradient: {
+          shadeIntensity: 1,
+          inverseColors: false,
+          opacityFrom: 0.5,
+          opacityTo: 0,
+          stops: [0, 90, 100],
+        },
+      },
+      yaxis: {
+        labels: {
+          formatter: function (val) {
+            return (val).toFixed(2);
+          },
+        },
+        title: {
+          text: "Price",
+        },
+      },
+      xaxis: {
+        type: "datetime",
+      },
+      tooltip: {
+        theme: "dark",
+        shared: false,
+        y: {
+          formatter: function (val) {
+            return (val).toFixed(0);
+          },
+          title: {
+            formatter: (seriesName) => seriesName,
+        },
+        },
+      },
+    },
   };
-    return (
-      <div className="app">
-        <div className="row">
-          <div className="mixed-chart">
-            <Chart
-              options={chart.options}
-              series={chart.series}
-              type="bar"
-              width="500"
-            />
-          </div>
-        </div>
-      </div>
-    );
-}
+
+  return (
+    <div id="chart">
+      <Chart
+        options={variant.options}
+        series={variant.series}
+        type="area"
+        height={350}
+        width={variant.options.chart.width}
+      />
+    </div>
+  );
+};
 
 export default StockChart;
