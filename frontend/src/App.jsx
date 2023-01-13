29c2fe9985c353055e8d579b1306a12691ab8acd
Prajwal Dhungana <prajwaldhungana164@gmail.com>
2023-01-13 16:01:05 +0545

    feat: Create status state
    feat: Create tickerSymbol state
    
    fix: Change url from '/items' to '/status'
    
    feat: Create function to get tickerSymbol
    
    style: Change styling and UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,18 @@ import Form from "./components/Form";
 import StockChart from "./components/Chart";
 
 function App() {
-  const [items, setItems] = useState([]);
+  const [status, setStatus] = useState("Offline");
+  const [tickerSymbol, setTickerSymbol] = useState("");
   const [date, setDate] = useState([]);
   const [closePrice, setClosePrice] = useState([]);
+  const [ready, setReady] = useState(false);
 
   axios.defaults.baseURL = "http://127.0.0.1:5000";
 
   useEffect(() => {
     async function fetchData() {
-      const result = await axios("/items");
-      setItems(result.data.items);
+      const result = await axios("/status");
+      setStatus(result.data.status);
     }
     fetchData();
   }, []);
@@ -21,21 +23,33 @@ function App() {
   const get_data = (get_data) => {
     setDate(get_data.data.date);
     setClosePrice(get_data.data.close);
+    if (date && closePrice !== []) setReady(true);
+  };
+
+  const get_ticker = (ticker) => {
+    setTickerSymbol(ticker);
   };
 
   return (
-    <div className="bg-slate-200 flex flex-col h-screen items-center justify-center">
-      <h1 className="text-6xl mb-5 text-slate-600 font-bold">Items</h1>
-      <ul className="list-disc mb-5">
-        {items.map((item) => (
-          <li className="text-slate-500 text-2xl" key={item}>
-            {item}
-          </li>
-        ))}
-      </ul>
-      <Form series={get_data} />
-      <StockChart date={date} close={closePrice} />
-      <p className="text-slate-400 text-sm">Fetched from backend server</p>
+    <div className="flex flex-col items-center">
+      <div className="border-2 border-teal-500 text-teal-500 font-bold rounded-full px-3 py-0 flex justify-center items-center gap-2 mt-4">
+        <div className="w-3 h-3 bg-teal-500 flex rounded-full"></div>
+        {status}
+      </div>
+      <h1 className="text-4xl mb-5 text-slate-600 font-bold mt-8">
+        Stock Price Predictor
+      </h1>
+      <Form className="mb-14" series={get_data} ticker={get_ticker} />
+      {ready === true ? (
+        <>
+          <StockChart date={date} close={closePrice} ticker={tickerSymbol} />
+          <p className="text-slate-400 text-sm mb-8">
+            Fetched from backend server
+          </p>
+        </>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
