2380a6a3b743b1267b28110650fec51264dabfdc
Prajwal Dhungana <prajwaldhungana164@gmail.com>
2023-03-16 00:26:01 +0545

    fix: Remove get_predicts function
    refactor: Change layout and structure of the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,58 +24,55 @@ const App = () => {
   }, []);
 
   const get_data = (get_data) => {
-    setDate(get_data.data[0].date);
-    setClosePrice(get_data.data[0].close);
+    setDate(get_data.data.date);
+    setClosePrice(get_data.data.close);
     if (date && closePrice != []) setReady(true);
-  };
-
-  const get_predicts = (get_data) => {
-    setPredictDate(get_data.data[1].date);
-    setPredictClosePrice(get_data.data[1].close);
-    if (predictDate && predictClosePrice != []) setReady(true);
   };
 
   const get_ticker = (ticker) => {
     setTickerSymbol(ticker);
   };
 
-  let trendChartMessage = "Stock Price Movement"
-  let predictionChartMessage = "Stock Price Prediction for the next 7 days"
+  let trendChartMessage = "Stock Trend";
+  let predictionChartMessage = "Stock Price Prediction for the next 7 days";
 
   return (
-    <div className="flex flex-col items-center">
-      {status == "Online" ? (
-        <div className="border-2 border-teal-500 text-teal-500 font-bold rounded-full px-3 py-0 flex justify-center items-center gap-2 mt-4">
-          <div className="w-3 h-3 bg-teal-500 flex rounded-full"></div>
-          {status}
+    <>
+      <div className="flex flex-col items-center justify-center">
+        <div className="p-4 mt-4">
+          {status == "Online" ? (
+            <div className="border-2 border-teal-500 text-teal-500 font-bold rounded-full px-3 py-0 flex justify-center items-center gap-2 top">
+              <div className="w-3 h-3 bg-teal-500 flex rounded-full"></div>
+              {status}
+            </div>
+          ) : (
+            <div className="border-2 border-red-500 text-red-500 font-bold rounded-full px-3 py-0 flex justify-center items-center gap-2 mt-4">
+              <div className="w-3 h-3 bg-red-500 flex rounded-full"></div>
+              {status}
+            </div>
+          )}
         </div>
-      ) : (
-        <div className="border-2 border-red-500 text-red-500 font-bold rounded-full px-3 py-0 flex justify-center items-center gap-2 mt-4">
-          <div className="w-3 h-3 bg-red-500 flex rounded-full"></div>
-          {status}
+        <div className="flex flex-col items-center">
+          <h1 className="text-4xl mb-5 text-slate-600 font-bold mt-8">
+            Stock Price Predictor
+          </h1>
+          <Form className="mb-14" series={get_data} ticker={get_ticker} />
+          {ready === true && (
+            <>
+              <StockChart
+                date={date}
+                close={closePrice}
+                ticker={tickerSymbol}
+                message={trendChartMessage}
+              />
+              <p className="text-slate-400 text-sm mb-8">
+                Fetched from backend server
+              </p>
+            </>
+          )}
         </div>
-      )}
-      <h1 className="text-4xl mb-5 text-slate-600 font-bold mt-8">
-        Stock Price Predictor
-      </h1>
-      <Form
-        className="mb-14"
-        series={get_data}
-        ticker={get_ticker}
-        predicts={get_predicts}
-      />
-      {ready === true ? (
-        <>
-          <StockChart date={date} close={closePrice} ticker={tickerSymbol} message={trendChartMessage} />
-          <StockChart date={predictDate} close={predictClosePrice} ticker={tickerSymbol} message={predictionChartMessage} />
-          <p className="text-slate-400 text-sm mb-8">
-            Fetched from backend server
-          </p>
-        </>
-      ) : (
-        ""
-      )}
-    </div>
+      </div>
+    </>
   );
 };
 
