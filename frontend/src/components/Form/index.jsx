3df4d58f55e467c39988336c7a1206b9bfbcabe2
Prajwal Dhungana <prajwaldhungana164@gmail.com>
2023-03-16 00:30:00 +0545

    feat: Implement Material UI components

--- frontend/src/components/Form/index.jsx
+++ frontend/src/components/Form/index.jsx
@@ -1,20 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-
-// CSS Modules, react-datepicker-cssmodules.css
-import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+import { DatePicker } from "@mui/x-date-pickers";
+import { Button, TextField, CircularProgress } from "@mui/material";
 
 export default function Form(props) {
   const [tickerSymbol, setTickerSymbol] = useState("");
-  const [startDate, setStartDate] = useState(new Date());
 
   const ticker = { tickerSymbol };
-  
+
   const handleSubmit = (event) => {
     event.preventDefault();
-
+    console.log(tickerSymbol)
     if (tickerSymbol !== "") {
       props.ticker(tickerSymbol);
       axios
@@ -22,7 +18,6 @@ export default function Form(props) {
         .then((response) => {
           console.log(response.data);
           props.series(response.data);
-          props.predicts(response.data);
         })
         .catch((error) => {
           console.error(error);
@@ -38,24 +33,13 @@ export default function Form(props) {
   return (
     <div className={`${props.className} form-container p-7`}>
       <form
-        className="flex flex-col gap-3"
+        className="flex gap-3"
         onSubmit={handleSubmit}
         method="POST"
       >
-        <input
-          className="px-5 py-3 uppercase w-[500px] placeholder:capitalize border-2 border-slate-400 outline-none"
-          type="text"
-          name="tickerSymbol"
-          placeholder="Enter the ticker symbol"
-          onChange={handleChange}
-        />
-        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
-        <button
-          className="bg-slate-500 text-white px-5 py-3 hover:bg-slate-600"
-          type="submit"
-        >
-          Forecast
-        </button>
+        <TextField id="outlined-basic" label="Ticker Symbol" variant="outlined" value={tickerSymbol} onChange={(e) => handleChange(e)} />
+        <DatePicker label="Target prediction date" />
+        <Button variant="contained" type="submit">Forecast</Button>
       </form>
     </div>
   );
