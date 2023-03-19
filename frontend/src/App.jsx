bece8c7dc451cebe0801e1877c4932200ef21ab8
Prajwal Dhungana <prajwaldhungana164@gmail.com>
2023-03-19 09:41:46 +0545

    feat: Add new statistics states and set stats data
    feat: Add table to display statistics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,12 @@ const App = () => {
   const [trainClosePrice, setTrainClosePrice] = useState([]);
   const [predictionDate, setPredictionDate] = useState([]);
   const [predictionClosePrice, setPredictionClosePrice] = useState([]);
+  const [trainAccuracy, setTrainAccuracy] = useState([]);
+  const [testAccuracy, setTestAccuracy] = useState([]);
+  const [trainMSE, setTrainMSE] = useState([]);
+  const [testMSE, setTestMSE] = useState([]);
+  const [trainRMSE, setTrainRMSE] = useState([]);
+  const [testRMSE, setTestRMSE] = useState([]);
   const [ready, setReady] = useState();
 
   axios.defaults.baseURL = "http://127.0.0.1:5000";
@@ -41,15 +47,23 @@ const App = () => {
     setTestClosePrice(get_data.test.close);
     setPredictionDate(get_data.prediction.date);
     setPredictionClosePrice(get_data.prediction.close);
+    setTrainAccuracy(get_data.train_accuracy);
+    setTestAccuracy(get_data.test_accuracy);
+    setTrainMSE(get_data.train_mse);
+    setTestMSE(get_data.test_mse);
+    setTrainRMSE(get_data.train_rmse);
+    setTestRMSE(get_data.test_rmse);
     if (
-      date && 
-      closePrice && 
-      testDate && 
-      testClosePrice && 
-      trainDate && 
-      trainClosePrice && 
-      predictionDate && 
-      predictionClosePrice != []) setReady(true);
+      date &&
+      closePrice &&
+      testDate &&
+      testClosePrice &&
+      trainDate &&
+      trainClosePrice &&
+      predictionDate &&
+      predictionClosePrice != []
+    )
+      setReady(true);
   };
 
   return (
@@ -73,16 +87,16 @@ const App = () => {
             <h1 className="text-4xl my-4 text-slate-600 font-bold text-center">
               Stock Price Predictor
             </h1>
-            <Form
-              className="mb-2"
-              ticker={get_ticker}
-              series={get_data}
-            />
+            <Form className="mb-2" ticker={get_ticker} series={get_data} />
           </div>
           <TradingViewWidget />
           {ready && (
-            <div id="chart-container" className="flex flex-col w-full px-4 md:px-12 lg:px-32">
-              <p className="mb-3 font-bold text-slate-600 text-xl text-center">
+          <>
+            <div
+              id="chart-container"
+              className="flex flex-col w-full px-4 md:px-12 lg:px-32"
+            >
+              <p className="mb-3 font-medium text-slate-600 text-3xl text-center p-4">
                 {tickerSymbol} Stock Prediction
               </p>
               <StockChart
@@ -98,10 +112,42 @@ const App = () => {
                 series="Trend"
                 title="Stock Trend"
               />
+            </div>
+            <div className="flex flex-col justify-between mb-20 w-full px-4 md:px-12 lg:px-32">
+                <p className="text-2xl text-slate-600 font-medium text-center mb-4 p-4">
+                  Statistics of the designed LSTM model
+                </p>
+                <table className="rounded-lg overflow-hidden table-auto min-w-full text-center font-medium">
+                  <thead className="border-b bg-slate-600 font-medium text-white dark:border-neutral-500 dark:bg-neutral-900">
+                    <tr>
+                      <th scope="col" class=" px-6 py-4">Statistics</th>
+                      <th scope="col" class=" px-6 py-4">Training</th>
+                      <th scope="col" class=" px-6 py-4">Testing</th>
+                    </tr>
+                  </thead>
+                  <tbody className="bg-slate-100">
+                    <tr className="border-b dark:border-neutral-500">
+                      <td className=" px-6 py-4">Accuracy</td>
+                      <td className=" px-6 py-4">{trainAccuracy + " %"}</td>
+                      <td className=" px-6 py-4 text-green-700">{testAccuracy + " %"}</td>
+                    </tr>
+                    <tr className="border-b dark:border-neutral-500">
+                      <td className=" px-6 py-4">Mean Squared Error</td>
+                      <td className=" px-6 py-4">{trainMSE}</td>
+                      <td className=" px-6 py-4 text-green-700">{testMSE}</td>
+                    </tr>
+                    <tr>
+                      <td className=" px-6 py-4">Root Mean Squared Error</td>
+                      <td className=" px-6 py-4">{trainRMSE}</td>
+                      <td className=" px-6 py-4 text-green-700">{testRMSE}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
               <p className="text-slate-400 text-sm mb-8 text-center">
                 Fetched from backend server
               </p>
-            </div>
+            </>
           )}
         </div>
       </div>
